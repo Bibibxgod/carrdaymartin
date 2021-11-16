bb6f9da2a0d610005d02373f0a2713ca58c9cf6a
Nikita <nakozhem@gmail.com>
2021-11-16 22:45:04 +0300

    alert 50%

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
+app.secret_key = 'zxczxczxczxc'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class ProductItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,46 +23,55 @@ class ProductItem(db.Model):
     def __repr__(self):
         return '<ProductItem %r>' % self.id
 
+
 @app.route('/')
 @app.route('/home')
 def index():
     Productitem = ProductItem.query.all()
     return render_template('index.html', Productitem=Productitem)
 
+
 @app.route('/products/leather')
 def productsleather():
     productitem = ProductItem.query.all()
     return render_template('products-leather.html', productitem=productitem)
+
 
 @app.route('/products/coat')
 def productscoat():
     productitem = ProductItem.query.all()
     return render_template('products-coat.html', productitem=productitem)
 
+
 @app.route('/products/hoof')
 def productshoof():
     productitem = ProductItem.query.all()
     return render_template('products-hoof.html', productitem=productitem)
+
 
 @app.route('/products/health')
 def productshealth():
     productitem = ProductItem.query.all()
     return render_template('products-health.html', productitem=productitem)
 
+
 @app.route('/products/<string:Type>/<string:id>')
-def product_card(Type,id):
+def product_card(Type, id):
     productitem = ProductItem.query.get(Type)
     productitem = ProductItem.query.get(id)
     Productitem = ProductItem.query.all()
     return render_template('product-card.html', productitem=productitem, Productitem=Productitem)
 
+
 @app.route('/history')
 def history():
     return render_template('history.html')
 
+
 @app.route('/explore')
 def explore():
     return render_template('explore.html')
+
 
 @app.route('/create', methods=['POST', 'GET'])
 def create():
@@ -72,8 +83,9 @@ def create():
         decription = request.form['decription']
         application = request.form['application']
 
-        productitem = ProductItem(name=name, Type=Type, size=size, smallDecription=smallDecription, decription=decription, application=application)
-        
+        productitem = ProductItem(name=name, Type=Type, size=size,
+                                  smallDecription=smallDecription, decription=decription, application=application)
+
         db.session.add(productitem)
         db.session.commit()
 
@@ -82,9 +94,11 @@ def create():
     else:
         return render_template('create.html')
 
+
 @app.route('/shops', methods=['POST', 'GET'])
 def shops():
     return render_template('shops.html')
+
 
 @app.route('/kontakts', methods=['POST', 'GET'])
 def kontakts():
@@ -93,27 +107,34 @@ def kontakts():
         email = request.form['email']
         telephone = request.form['telephone']
         message = request.form['message']
-        
+
         msg = MIMEMultipart()
-        
+
         from_email = 'carrdaymartinru@gmail.com'
         password = '$VFR4567ygv$'
         to_email = 'nnakozhemm@gmail.com'
-        text = 'Почта: ' + email + '\n' + 'Телефон: ' + telephone + '\n' + 'Заголовок: ' + name + '\n' + 'Сообщение: ' + message + '\n'
-        
+        text = 'Почта: ' + email + '\n' + 'Телефон: ' + telephone + '\n' + \
+            'Заголовок: ' + name + '\n' + 'Сообщение: ' + message + '\n'
+
+        if len(name or email or telephone or message) == 0:
+            flash('Это поле обязательно')
+            return redirect(request.url)
+
         msg.attach(MIMEText(text, 'plain'))
-        
         server = smtplib.SMTP('smtp.gmail.com: 587')
         server.starttls()
         server.login(from_email, password)
         server.sendmail(from_email, to_email, msg.as_string())
         server.quit()
+            
+        
+        
+        flash("Вы успешно отправили свое сообщение, мы ответим вам в ближайшее время.")
+        return redirect(request.url)
 
-        return "successfully sent email to:"
-    
     else:
         return render_template('kontakts.html')
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
